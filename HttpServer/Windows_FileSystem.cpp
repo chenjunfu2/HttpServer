cc697b5f7c62dd7b42ba0761b79df8714672330b
chenjunfu2 <64954184+chenjunfu2@users.noreply.github.com>
2026-01-10 19:32:37 +0800

    1

--- HttpServer/Windows_FileSystem.cpp
+++ HttpServer/Windows_FileSystem.cpp
@@ -19,15 +19,9 @@ ErrorMessage OpenFileAndMapping(const wchar_t *pwcFileName, void *&pFile, uint64
 		CloseHandle(hReadFile);//关闭输入文件
 		return ErrorMessage(dwLastError);
 	}
-	u64FileSize = liFileSize.QuadPart;
 
-	//判断文件为空
-	if (liFileSize.QuadPart == 0)
-	{
-		auto dwLastError = GetLastError();
-		CloseHandle(hReadFile);//关闭输入文件
-		return ErrorMessage(dwLastError);
-	}
+	//通过参数返回
+	u64FileSize = liFileSize.QuadPart;
 
 	//创建文件映射对象
 	HANDLE hFileMapping = CreateFileMappingW(hReadFile, NULL, PAGE_READONLY, 0, 0, NULL);
@@ -44,22 +38,29 @@ ErrorMessage OpenFileAndMapping(const wchar_t *pwcFileName, void *&pFile, uint64
 	if (!lpReadMem)
 	{
 		auto dwLastError = GetLastError();
-		CloseHandle(hFileMapping);//关闭文件映射对象
+		CloseHandle(hFileMapping);//关闭文件映射对象  注：此处不用关闭输入文件，前面已经关闭过了
 		return ErrorMessage(dwLastError);
 	}
 	CloseHandle(hFileMapping);//关闭文件映射对象
 
 	pFile = (void *)lpReadMem;
+
+	//最后可以关闭所有文件对象（除了映射地址外）而不会出问题的原因是：
+	//Win底层引用计数会根据多次打开累计。前面的关闭只是减少引用计数，
+	//但是在减少之前已经通过其它API增加了一次引用，这就导致对象被延长生命周期，
+	//而不是立刻销毁。这样，最后那个指针持有前面所有文件映射对象的引用，
+	//而文件映射对象持有文件的引用。最后，回收的时候只需要关闭指针即可连续释放所有引用。
 	return {};
 }
 
-ErrorMessage UnMappingAndCloseFile(uint8_t *pFileClose)
+ErrorMessage UnMappingAndCloseFile(void *&pFileClose)
 {
 	if (!UnmapViewOfFile(pFileClose))
 	{
 		return ErrorMessage(GetLastError());
 	}
 
+	pFileClose = NULL;
 	return {};
 }
 
